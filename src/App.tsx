99f6964576af04b52579707dcc54256159c0e456
craig-coder <craig@waterfallfittedfurniture.co.uk>
2025-09-28 10:33:58 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import HeartbeatTest from "./components/HeartbeatTest";
-import React from "react"
+import React from "react";
 const SYNC_URL = import.meta.env.VITE_SYNC_URL; // Apps Script web app URL
 
-const DEFAULT_SHEET_URL = "https://docs.google.com/spreadsheets/d/1Meojz6Ob41qPc2m-cvws24d1Zf7TTfqmo4cD_AFUOXU/edit?gid=270301091#gid=270301091"
+const DEFAULT_SHEET_URL =
+  "https://docs.google.com/spreadsheets/d/1Meojz6Ob41qPc2m-cvws24d1Zf7TTfqmo4cD_AFUOXU/edit?gid=270301091#gid=270301091";
 
 // … all your functions here (unchanged) …
 
@@ -26,10 +27,15 @@ export default function App() {
           {/* … existing table … */}
         </section>
 
-        {/* 👇 Added HeartbeatTest here */}
         <section className="bg-white rounded-2xl shadow p-4">
           <HeartbeatTest />
         </section>
 
         <footer className="text-xs text-gray-500 text-center">
-          Weights, mandato
+          Weights, mandatory stages, and Google Apps Script persistence can be
+          added next.
+        </footer>
+      </div>
+    </div>
+  );
+}
