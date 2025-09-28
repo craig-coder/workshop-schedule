109c0970b9c5f7de723216825f60191dc05e28bb
craig-coder <craig@waterfallfittedfurniture.co.uk>
2025-09-28 14:23:30 +0100

    Update sheets.js

--- api/sheets.js
+++ api/sheets.js
@@ -1,7 +1,8 @@
 // Serverless proxy to Google Apps Script to avoid CORS issues.
 export default async function handler(req, res) {
   // Use your working Apps Script URL:
-  const API = "https://script.google.com/macros/s/AKfycbyN8qR8nXfweKin6fZmsh6Ykka9fmikAg6wI4sH26k4XFck6TZjn2IiKfqyCKDx7BAc7g/exec";
+const API = "https://script.google.com/macros/s/AKfycbyha1bpsQm0lBQU5tJE0L4vCEd8yJlJNFoZF5b5PqZMudb9RlF8Run7JYMFzw2OSWQGIQ/exec";
+
 
   try {
     let url = API;
